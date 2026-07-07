1d4177b1b643e780b0fc259cb1d49889e7720cfb
Kuriton77 <kamikuriton@gmail.com>
2026-07-07 18:24:35 +0200

    Update start.bat

--- Nightfall/start.bat
+++ Nightfall/start.bat
@@ -1,27 +1,8 @@
 @echo off
-title Nightfall
 cd /d "%~dp0"
 
-echo ==========================
-echo      Starting Nightfall
-echo ==========================
-echo.
+start "" http://localhost:8123
 
-:: Installiert fehlende Pakete (falls node_modules fehlt)
-if not exist node_modules (
-    echo Installing dependencies...
-    call npm install
-    if errorlevel 1 (
-        echo.
-        echo Failed to install dependencies.
-        pause
-        exit /b
-    )
-)
+python -m http.server 8123
 
-:: Startet das Projekt
-call npm start
-
-echo.
-echo Nightfall has exited.
 pause
